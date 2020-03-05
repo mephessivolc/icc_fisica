322db975ec7ad82c2eabde1a88f782750f6cf84c
clovis caface <clovis.caface@gmail.com>
2020-03-05 20:20:08 -0300

    Primeiro programa em linguagem c

---
+++ primeiro.c
@@ -0,0 +1,13 @@
+//1: biblioteca
+//2: escrever ("olá mundo")
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+//printf("Olá Mundo!!!\n");
+//printf("soma %d", 2+2);
+//printf("subtração %d", 2-2);
+printf("multiplicação %d", 12/(3+3)*5);
+//printf("divisão %d", 2/2);
+}
