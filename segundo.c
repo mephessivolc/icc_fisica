e35037b9829f756fe1eaf022641a4cea8659cf40
clovis caface <clovis.caface@gmail.com>
2020-03-07 16:09:35 -0300

    Primeira instrucao

---
+++ segundo.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+    printf("Olá Mundo!!!\n");
+    // calculos
+    printf("soma: %d\n", 2+2);
+    printf("multiplicacao: %d\n", 45*3);
+    printf("subtracao: %d\n", 30-10);
+    printf("divisao: %d\n", 12/5);
+}
+
+
